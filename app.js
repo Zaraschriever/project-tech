afcf74e3cb5741a602931d234e9b05b1ddce29c2
Zaraschriever <zaraschriever@DESKTOP-QPD94T3.localdomain>
2021-03-08 18:23:35 +0100

    Remove the now ignored directory node_modules

--- app.js
+++ app.js
@@ -1,84 +1,54 @@
 const express = require('express');
 
-const app = express();
-const port = 9020;
-const path = require('path');
-const expbs = require('express-handlebars');
+const slug = require('slug');
 
-const mongoose = require('mongoose');
-const { schema } = require('./models/user.model');
+const app = express();
+const expbs = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const { MongoClient } = require('mongodb');
+
+const port = 9020;
+
 require('dotenv').config();
 
-const uri =
-  'mongodb+srv://dbZara:Joepie1805@cluster0.cgla0.mongodb.net/matchingapplication?retryWrites=true&w=majority';
+let db = null;
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(uri)
-  .then(() => console.log('DB connection succesfull'))
-  .catch((err) => console.error(err));
+async function connectDB() {
+  // get URI form env file
+  const uri =
+    'mongodb+srv://dbZara:Joepie1805@cluster0.cgla0.mongodb.net/matchingapplication?retryWrites=true&w=majority';
+  // make connection to DB
+  const options = { useUnifiedTopology: true };
+  const client = new MongoClient(uri, options);
+  await client.connect();
+  db = await client.db(process.env.DB_NAME);
+}
+connectDB();
+try {
+  console.log('Connected to database!');
+} catch (error) {
+  console.log(error);
+}
 
-// express
-app.use('/static', express.static(path.join(__dirname, 'static/public')));
+// const uri =
+//   'mongodb+srv://dbZara:Joepie1805@cluster0.cgla0.mongodb.net/matchingapplication?retryWrites=true&w=majority';
+
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(uri)
+//   .then(() => console.log('DB connection succesfull'))
+//   .catch((err) => console.error(err));
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', expbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/views'));
 
-// schema
-const { Schema } = mongoose;
-const BlogPostSchema = new Schema({
-  title: String,
-  body: String,
-  date: {
-    type: String,
-    default: Date.now(),
-  },
-});
-
-// model
-const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
-
-const data = {
-  title: 'hello world',
-  body: 'dhcbhs haxvhdvhsx hxhvg',
-};
-
-const newBlogPost = new BlogPost(data);
-
-newBlogPost.save((error) => {
-  if (error) {
-    console.log('Oops, something happend');
-  } else {
-    console.log('Data has been saved!');
-  }
-});
-
-
-
-
-
-// const createUser = mongoose.model('createUser', createUserSchema);
-
-
-// db.createUser( {
-//   user : "Joep",
-//   pwd : "1234",
-//   roles : ["readWrite", "dbAdmin"]
-// });
-
-// const createUser = new createUser(data);
-
-
-
-
-
 // homepage
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home Page' });
-});
-
-app.get('/login', (req, res) => {
-  res.render('login', { title: 'Login' });
 });
 
 app.get('/matches', (req, res) => {
@@ -93,8 +63,38 @@ app.listen(port, () => {
   console.log('Server is starting at port', port);
 });
 
+// profiel pagina
+
 // 404 page
 app.use((req, res) => {
   res.status(404).send('404 Not Found');
 });
 
+// // schema
+// const { Schema } = mongoose;
+// const BlogPostSchema = new Schema({
+//   title: String,
+//   body: String,
+//   date: {
+//     type: String,
+//     default: Date.now(),
+//   },
+// });
+
+// model
+// const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+
+// const data = {
+//   title: 'hello world',
+//   body: 'dhcbhs haxvhdvhsx hxhvg',
+// };
+
+// const newBlogPost = new BlogPost(data);
+
+// newBlogPost.save((error) => {
+//   if (error) {
+//     console.log('Oops, something went wrong');
+//   } else {
+//     console.log('Data has been saved!');
+//   }
+// });
--- database.js
+++ database.js
@@ -1,24 +1,45 @@
-const MongoClient = require('mongodb');
-require('dotenv').config();
+// let db = null;
+// // function connectDB
+// async function connectDB() {
+//   // get URI from .env file
+//   const uri = process.env.DB_URI;
+//   // make connection to database
+//   const options = { useUnifiedTopology: true };
+//   const client = new MongoClient(uri, options);
+//   await client.connect();
+//   db = await client.db(process.env.DB_NAME);
+// }
+// connectDB()
+//   .then(() => {
+//     // if succesfull connections is made, show a message
+//     console.log('We have a connection to Mongo!');
+//   })
+//   .catch((error) => {
+//     // if connnection is unsuccesful, show errors
+//     console.log(error);
+//   });
 
-const uri = process.env.ATLAS_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
-    // Establish and verify connection
-    // await client.db('admin').command({ ping: 1 });
-    console.log('Connected successfully to server');
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// const MongoClient = require('mongodb');
+// require('dotenv').config();
+
+// const uri = process.env.ATLAS_URI;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server
+//     await client.connect();
+//     // Establish and verify connection
+//     // await client.db('admin').command({ ping: 1 });
+//     console.log('Connected successfully to server');
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 // require('dotenv').config();
 
